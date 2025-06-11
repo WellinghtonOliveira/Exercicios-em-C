7c418e9a2892a1702fff36f9c1df9ea18c4a32b1
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-06-11 14:46:39 -0300

    Update lendo.c

--- Lendo Arquivos/lendo.c
+++ Lendo Arquivos/lendo.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
 int main() {
-	FILE *text;
 	
-	text = fopen("escrevendoLendo.txt", "w");
-	fprintf(text, "Escrito em 'C'");
+	// Escreve no arquivo
+	FILE *text;
+	text = fopen("escrevendoLendo.txt", "a");
+	fprintf(text, "\nMineca dicatibiriba");
+	fclose(text);
+	
+	// le o arquivo
+	text = fopen("escrevendoLendo.txt", "r");
+	char meuTexto[100];
+	while (fgets(meuTexto, 100, text)) {
+		printf("%s", meuTexto);
+	}
 	fclose(text);
 	
 	return 0;
