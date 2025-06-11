51e54181088c4f13c4a289d19a3ab7361f598021
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-06-11 14:58:54 -0300

    Update estruturas em c.c

--- Estruturas/estruturas em c.c
+++ Estruturas/estruturas em c.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+struct myStruct {
+	int num;
+	char letter;
+};
+
 int main() {
 	
+	struct myStruct s1;
 	
+	s1.num = 15 + 2;
+	s1.letter = 'G';
+	
+	printf("O numero: %d\n", s1.num);
+	printf("A letra: %c\n", s1.letter);
 	
 	return 0;
 }
