c12c8c6f1273b2db6cc4041418f0e9a6e19d0fa1
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-06-13 15:04:01 -0300

    Update media.c

--- Exercicios/media/media.c
+++ Exercicios/media/media.c
@@ -1,28 +1,49 @@
 #include <stdio.h>
 
-char gradeFuncao(double avg) {
-	if (avg >= 90) return 'A';
-	else if (avg >= 80) return 'B';
-	else if (avg >= 70) return 'C';
-	else if (avg >= 60) return 'D';
+char gradeFuncao(double str) {
+	if (str >= 90) return 'A';
+	else if (str >= 80) return 'B';
+	else if (str >= 70) return 'C';
+	else if (str >= 60) return 'D';
 	else return 'F';
 }
 
 int main() {
 	
-	int nota;
-	
-	printf("Digite sua nota para calcularmaos: ");
-	scanf("%d", &nota);
-	
+	char continuar = '0';
+
 	do {
-		if (nota < 1 || nota > 5) {
-			printf("\nNota invalida digite um numero entre 1 a 5");
-			scanf("Digite a nota valida: %d", &nota);
-		}
-	}while (nota > 1 || nota < 5);
+		continuar = '0';
+		int materia;
+		double sum = 0, nota, media;
+		
+		printf("\nDigite a quantia de materias: ");
+		scanf("%d", &materia);
 	
-	printf("\nSua nota media e ==> %d", nota);
+		for (int i = 0; i < materia; i++) {
+			printf("\nNota - Materia %d => ", + 1);
+			scanf("%lf", &nota);
+			sum += nota;
+		}
+	
+		media = sum / materia;
+		printf("\n\nSua media e => %.2lf", media);
+		printf("\nVoce tirou um => %c", gradeFuncao(media));
+		
+		printf("\n\nVoce deseja refazer o calculo, [S|N] => ");
+		scanf("%c", &continuar);
+	
+		if (
+		continuar != 's'||
+		continuar != 'S'||
+		continuar != 'n'||
+		continuar != 'N'
+		) {
+			printf("\n\nLetra invalida");
+			return 1;
+		}
+	
+	} while (continuar != 'S' || continuar != 's');
 	
 	return 0;
 }
