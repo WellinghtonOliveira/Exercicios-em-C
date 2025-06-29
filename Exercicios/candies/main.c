7c05ef68adb81f8aa99a831b27a627d377a6a6d0
Wellinghton O. <wellinghtonbatistahotmail@gmail.com>
2025-06-29 16:09:55 -0300

    Update main.c

--- Exercicios/candies/main.c
+++ Exercicios/candies/main.c
@@ -1,36 +1,50 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <windows.h>
 
-bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize) {
+bool *kidsWithCandies(int *candies, int candiesSize, int extraCandies, int *returnSize)
+{
 
     char values[candiesSize];
-    
-    int i = 0;
+
+    int i = 0, k;
 
     while (i < candiesSize)
     {
         int soma = candies[i] + extraCandies;
 
-        
-
-        if (soma > candies[i])
+        for (int j = 0; j < candiesSize; j++)
         {
-            printf(" %d\n", i);
+            if (soma > candies[j])
+            {
+                values[j] = 't';
+            }else
+            {
+                values[j] = 'f';
+            }
         }
-        
+
+        for (k = 0; k < candiesSize; k++) 
+        {
+            if (values[k] == 't')
+            {
+            }
+        }
 
         i++;
     }
-    
+
+    printf("\n\n%s", values);
 }
 
-int main() {
+int main()
+{
 
-    int candies[5] = { 2, 10, 3, 1, 4 };
+    int candies[5] = {2,3,5,1,3};
     int extraCandie = 3;
     int candieSize = sizeof(candies) / sizeof(candies[0]);
 
-    kidsWithCandies(candies, candieSize, extraCandie, 0 );
+    kidsWithCandies(candies, candieSize, extraCandie, 0);
 
     return 0;
 }
