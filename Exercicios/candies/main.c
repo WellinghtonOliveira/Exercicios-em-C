31babc74a564c86f36f11b837312c0876f420c11
Wellinghton O. <wellinghtonbatistahotmail@gmail.com>
2025-06-30 00:43:43 -0300

    Update main.c

--- Exercicios/candies/main.c
+++ Exercicios/candies/main.c
@@ -1,50 +1,53 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <windows.h>
+#include <string.h>
+#include <stdlib.h>
 
 bool *kidsWithCandies(int *candies, int candiesSize, int extraCandies, int *returnSize)
 {
+    bool *arrayValores = malloc(candiesSize * sizeof(bool));
 
-    char values[candiesSize];
+    int totalCandies;
+    int i, k, cont;
 
-    int i = 0, k;
-
-    while (i < candiesSize)
+    for (i = 0; i < candiesSize; i++)
     {
-        int soma = candies[i] + extraCandies;
+        totalCandies = 0;
+        cont = 0;
 
-        for (int j = 0; j < candiesSize; j++)
+        totalCandies = candies[i] + extraCandies;
+
+        for (k = 0; k < candiesSize; k++)
         {
-            if (soma > candies[j])
+            if (totalCandies >= candies[k])
             {
-                values[j] = 't';
-            }else
-            {
-                values[j] = 'f';
+                cont++;
             }
         }
 
-        for (k = 0; k < candiesSize; k++) 
-        {
-            if (values[k] == 't')
-            {
-            }
-        }
-
-        i++;
+        arrayValores[i] = (cont == candiesSize);
     }
 
-    printf("\n\n%s", values);
+    *returnSize = candiesSize;
+    return  arrayValores;
 }
 
 int main()
 {
 
-    int candies[5] = {2,3,5,1,3};
-    int extraCandie = 3;
+    int tamanho;
+    int candies[3] = {12, 1, 12};
+    int extraCandie = 10;
     int candieSize = sizeof(candies) / sizeof(candies[0]);
 
-    kidsWithCandies(candies, candieSize, extraCandie, 0);
+    bool *resultado = kidsWithCandies(candies, candieSize, extraCandie, &tamanho);
+
+    for (int i = 0; i < tamanho; i++)
+    {
+        printf("%s\n", resultado[i] ? "true" : "false");
+    }
+
+    free(resultado);
 
     return 0;
 }
