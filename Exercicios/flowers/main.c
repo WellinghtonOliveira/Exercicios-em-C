9fcd59b58bd86aebb93ccab8ae30e95c9e51588b
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-07-01 15:49:45 -0300

    Update main.c

--- Exercicios/flowers/main.c
+++ Exercicios/flowers/main.c
@@ -2,41 +2,31 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-bool *canPlaceFlowers(int* flowerbed, int flowerbedSize, int n) {
-    bool *flowerValue = malloc(flowerbedSize * sizeof(bool));
-    
-    int i, j = 0, k = 0, cont = 0;
-
-    for (i = 0; i < flowerbedSize; i++)
+bool canPlaceFlowers(int *flowerbed, int flowerbedSize, int n)
+{
+    int c = 0;
+    for (int i = 0; i < flowerbedSize; i++)
     {
-        if (flowerbed[i] == )
+        if (flowerbed[i] == 0 &&
+            (i == 0 || flowerbed[i - 1] == 0) &&
+            (i == flowerbedSize - 1 || flowerbed[i + 1] == 0))
         {
-            /* code */
+            c++;
+            flowerbed[i] = 1;
         }
-        
-        j = flowerbed[i];
-        if (flowerbed[i] == j)
-        {
-            cont++;
-        }
-        
     }
-
-
-    printf("cont ==> %d k ==> %d \n", cont, k);
-    return flowerValue;
+    return c >= n;
 }
 
-int main() {
-
-    int flowerbed[5] = {1,1,0,0,1};
+int main()
+{
+    int flowerbed[5] = {0, 0, 0, 0, 1};
     int flowerbedSize = sizeof(flowerbed) / sizeof(flowerbed[0]);
     int n = 1;
 
-    bool *value = canPlaceFlowers(flowerbed, flowerbedSize, n);
+    bool value = canPlaceFlowers(flowerbed, flowerbedSize, n);
 
-    printf("%s", value[0] ? "true" : "false");
-    free(value);
+    printf("%s\n", value ? "true" : "false");
 
     return 0;
 }
