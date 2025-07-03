4721c0abd5e3fa79be1a757e11421e3a416b7686
Wellinghton O. <wellinghtonbatistahotmail@gmail.com>
2025-07-03 19:50:10 -0300

    Update main.c

--- Exercicios/arrays/main.c
+++ Exercicios/arrays/main.c
@@ -8,14 +8,14 @@ char* mergeAlternately(char* word1, char* word2) {
     int tamanhoJuntos = tamanhoWord1 + tamanhoWord2;
 
     // aloca espaço na heap para a nova string
-    char* texto = malloc(tamanhoJuntos + 1); // +1 para o '\0'
+    char* texto = malloc(tamanhoJuntos + 1); // +1 para o  '\0'
     if (texto == NULL) {
         printf("Erro ao alocar memória\n");
         return NULL;
     }
 
     int i = 0, j = 0, k = 0;
-
+ 
     while (i < tamanhoWord1 || j < tamanhoWord2) {
         if (i < tamanhoWord1)
             texto[k++] = word1[i++];
@@ -35,6 +35,7 @@ int main() {
         printf("Resultado: %s\n", resultado);
         free(resultado); // libera a memória
     }
-
+ 
     return 0;
 }
+ 
