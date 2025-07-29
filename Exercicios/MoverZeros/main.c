84ad8d271459eeaf610ea60514640a226fb4c96b
Wellinghton O. <wellinghtonbatistahotmail@gmail.com>
2025-07-29 16:20:04 -0300

    Update main.c

--- Exercicios/MoverZeros/main.c
+++ Exercicios/MoverZeros/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+void moveZeroes(int* nums, int numsSize) {
+    printf("%d", nums);
+    printf("\n%d", numsSize);
+}
+
 int main() {
+    int nums[5] = { 0, 1, 2, 0, 3 };
+    
+    moveZeroes(nums, 5);
     return 0;
 }
