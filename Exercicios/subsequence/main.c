9aaf708e8e94def3b8f846a1bbceffac9c7ae431
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-08-13 15:58:00 -0300

    Update main.c

--- Exercicios/subsequence/main.c
+++ Exercicios/subsequence/main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
-#include 
+#include <stdbool.h>
 
-bool isSubsequence(char* s, char* t) {
-    
+bool isSubsequence(char *s, char *t)
+{
+    for (int i = 0; i < (sizeof(s) / sizeof(s[0])); i++)
+    {
+        printf("%c", s[i]);
+    }
+
+    printf("%d", sizeof(s) / sizeof(s[0]));
 }
 
-int main() {
+int main()
+{
 
+    char s[] = "abc", p[] = "aqbtc";
+
+    isSubsequence(s, p);
 
     return 0;
 }
