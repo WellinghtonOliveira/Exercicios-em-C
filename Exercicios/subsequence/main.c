7cdcfc756fe3f1de97a53899e83571d1c6b67900
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-08-14 10:50:44 -0300

    Update main.c

--- Exercicios/subsequence/main.c
+++ Exercicios/subsequence/main.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 bool isSubsequence(char *s, char *t)
 {
-    for (int i = 0; i < (sizeof(s) / sizeof(s[0])); i++)
+    int tam_s = strlen(s);
+    int tam_t = strlen(t);
+    int i, k = 0, j = 0;
+
+    char tempCopy[tam_s];
+
+    for (i = 0; i < tam_t; i++)
     {
-        printf("%c", s[i]);
+        if (s[j] == t[i]) {
+            tempCopy[j] = s[j];
+            j++;
+        }
     }
 
-    printf("%d", sizeof(s) / sizeof(s[0]));
+    for (i = 0; i < tam_s; i++)
+    {
+        printf("%c", tempCopy[i]);
+        if (tempCopy[i] == s[i])
+        {
+            k++;
+        }
+    }
+    
+    return k == tam_s ? printf("\n\n%s", "verdadeiro") : printf("\n\n%s", "falso"); 
 }
 
 int main()
 {
 
-    char s[] = "abc", p[] = "aqbtc";
+    char s[] = "axc", p[] = "ahbgdc";
 
     isSubsequence(s, p);
 
