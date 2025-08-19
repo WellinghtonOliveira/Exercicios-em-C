74665c148dc3266dd7c97b1eea88f31e98c43c74
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-08-19 16:05:45 -0300

    Update main.c

--- malwere/keylogger/main.c
+++ malwere/keylogger/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <conio.h>
 
-int main() {
-    printf("teste");
+int main()
+{
+
+    char tecla;
+
+    printf("Precione ESC para sair:\n");
+
+    while (1)
+    {
+        if (_kbhit())
+        {
+            tecla = _getch();
+            printf("Tecla pressionada: %c\n\n", tecla);
+
+            if (tecla == 27)
+            {
+                break;
+            }
+        }
+    }
 
     return 0;
 }
