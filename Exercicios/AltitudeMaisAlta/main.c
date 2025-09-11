dfac5cbdefc76f6e7515db947d5a80269487d74f
Wellinghton O. <wellinghtonbatistahotmail@gmail.com>
2025-09-10 21:04:07 -0300

    Update main.c

--- Exercicios/AltitudeMaisAlta/main.c
+++ Exercicios/AltitudeMaisAlta/main.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 
-int main() {
-    int a[] = { 1, 23, 21, 2, -4};
+int largestAltitude();
 
-    printf("teste");
+int main() {
+
+
+    int gainSize = 5;
+    int gain[5] = {-5,1,5,0,-7};
+    
+    printf("%d", largestAltitude(gain, gainSize));
 
     return 0;
+}
+
+
+int largestAltitude(int* gain, int gainSize) {
+    int altitude = 0;
+    int maxAltitude = 0;
+
+    for (int i = 0; i < gainSize; i++) {
+        altitude += gain[i];
+        if (altitude > maxAltitude)
+            maxAltitude = altitude;
+    }
+
+    return maxAltitude;
 }
