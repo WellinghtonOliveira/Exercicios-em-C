a58bac5a15588c784738b6ee14508ac85f7d1063
Wellinghton O. <wellinghtonbatistahotmail@gmail.com>
2025-09-20 16:51:55 -0300

    Update main.c

--- Exercicios/indeice dinamico/main.c
+++ Exercicios/indeice dinamico/main.c
@@ -17,7 +17,7 @@ int pivotIndex(int* nums, int numsSize) {
 // Percorrer o array mantendo a soma da esquerda (começa em 0).
 // Em cada posição i, verificamos se:
 // soma_esquerda == soma_total - soma_esquerda - nums[i]
-// soma_esquerda: soma dos elementos à esquerda.
+// soma_esquerda: soma dos elementos à esquerda
 // soma_total - soma_esquerda - nums[i]: soma dos elementos à direita.
 // Se for igual → retornamos o índice i.
 // Caso termine o loop sem encontrar → retorna -1
