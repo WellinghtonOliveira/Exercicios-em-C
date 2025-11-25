3853c43c68dd2f548bb88091f7cb15c37048fb35
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-11-25 09:48:19 -0300

    Update 001.cpp

--- Exercicios/Testes/001.cpp
+++ Exercicios/Testes/001.cpp
@@ -3,31 +3,36 @@
 
 int main() {
 	int tamanhoQuadrado = 20;
-	int areaMatriz[tamanhoQuadrado][tamanhoQuadrado];
+	char areaMatriz[tamanhoQuadrado][tamanhoQuadrado];
 	int k = 0, j = 0, v = 0;	
 	
 	
 	while (true) {
+		
 		// Escreve na matriz
 		for (int a = 0; a < tamanhoQuadrado; a++) {
 			for (int b = 0; b < tamanhoQuadrado; b++) {
 				if (v == 1) {
-					areaMatriz[k][j] = 8;
+					areaMatriz[k][j] = '0';
+					continue;
 				}
 				
-				areaMatriz[a][b] = 0;
+				areaMatriz[a][b] = '-';
 			}
 		}
-	
+		
+		if (v == 1) {
+			k++;
+		}
+		
 		// Desenhar na tela
 		for (int a = 0; a < tamanhoQuadrado; a++) {
 			printf("   ");
 			for (int b = 0; b < tamanhoQuadrado; b++) {			
-				printf("%d ", areaMatriz[a][b]);
+				printf("%c ", areaMatriz[a][b]);
 			}
 			printf("\n");
 		}
-		
 		
 		v = 1;
 		Sleep(1000);
