e7ee1168d353ac3724a89d5ee1af302f9efb6cc5
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-11-28 13:33:25 -0300

    Update Jogo da velha - 002.cpp

--- Exercicios/Testes/Jogo da velha - 002.cpp
+++ Exercicios/Testes/Jogo da velha - 002.cpp
@@ -1,20 +1,36 @@
 #include <stdio.h>
+#include <conio.h>
 #include <iostream>
 #include <windows.h>
 
-void desenhaTela(int);
+void desenhaTela(char);
 void conf_tela(); // Configuração inicial da tela
+void attInputs();
+void attPosPlayer(char);
+
+const int tamanho = 42; // 42 tamanho padrão
+
+int posPlayer[3][3] = {};
+int coordenadasPlayer[2] = {};
+
+char tela[tamanho][tamanho] = {};
+
+
 
 int main() {
+	attPosPlayer('i');
+	
 	conf_tela();
-	desenhaTela(42); // 42 tamanho padrao
+	
+	attInputs();
 	
 	system("pause");
 	return 0;
 }
 
-void desenhaTela(int tamanho) {
-	char tela[tamanho][tamanho] = {};
+
+void desenhaTela(char input = 'p') {	
+	char tecla = input;
 	
 	int div = tamanho / 3;
 	
@@ -29,16 +45,6 @@ void desenhaTela(int tamanho) {
 			}else {
 				tela[a][b] = ' ';
 			}
-			
-			if (b == div + 1 && a >= div + 1 && a <= (div * 2) - 1 ||
-			    b == (div * 2) - 1 && a >= div + 1 && a <= (div * 2) - 1 ||
-				a == div + 1 && b >= div + 1 && b <= (div * 2) - 1 ||
-				a == (div * 2) - 1 && b >= div + 1 && b <= (div * 2) - 1) {
-				
-				tela[a][b] = 'Z';
-			}
-		
-			
 		}
 	}
 
@@ -48,10 +54,125 @@ void desenhaTela(int tamanho) {
 		printf("				");
 		for(int b = 0; b < tamanho; b++) {
 			
+			
+			if (tecla == 'w' || tecla == 'W') {
+				if (b == div + 1 && a >= 0 && a <= div - 1 ||
+					b == (div * 2) - 1 && a >= 0 && a <= div - 1 ||
+					a == 0 && b >= div + 1 && b <= (div * 2) - 1 ||
+					a == div - 1 && b >= div + 1 && b <= (div * 2) - 1) {
+					
+					printf("- ");
+					//tela[a][b] = '-';
+					continue;	
+				}
+			}
+				
+			// meio
+			if (b == div + 1 && a >= div + 1 && a <= (div * 2) - 1 ||
+			    b == (div * 2) - 1 && a >= div + 1 && a <= (div * 2) - 1 ||
+				a == div + 1 && b >= div + 1 && b <= (div * 2) - 1 ||
+				a == (div * 2) - 1 && b >= div + 1 && b <= (div * 2) - 1) {
+				
+				printf("- ");
+				//tela[a][b] = '-';
+				continue;
+			}
+		
 			printf("%c ", tela[a][b]);	
 		}
 		printf("\n");
 	}
+}
+
+void attInputs() {
+	char tecla;
+	
+	while (true) {
+		
+		if (kbhit()) {
+			tecla = getch();
+			
+			switch (tecla) {
+				case 'w': case 'W':
+					attPosPlayer('w');
+					break;
+				case 's': case 'S':
+					attPosPlayer('s');
+					break;
+				case 'a': case 'A':
+					attPosPlayer('a');
+					break;
+				case 'd': case 'D':
+					attPosPlayer('d');
+					break;
+				
+				default:
+					continue;
+			}
+		
+			tecla = '\0';
+		}
+		
+		Sleep(100);
+	}
+}
+
+void attPosPlayer(char conf = 'p') {
+	// configuração de inicialização
+	if (conf == 'i') {
+		for (int a = 0; a < 3; a++) {
+			for (int b = 0; b < 3; b++) {
+				posPlayer[a][b] = 0;
+					
+				if (a == 1 && b == 1) {
+					posPlayer[a][b] = 1;	
+				}
+			}
+		}
+	}
+	
+	
+	for (int a = 0; a < 3; a++) {
+		for (int b = 0; b < 3; b++) {
+			
+			switch (conf){
+				case 'w': case 'W':
+					if (posPlayer[a][b] == 1 && a > 0) {
+							posPlayer[a][b] = 0;
+							posPlayer[a - 1][b] = 1; 
+						}
+					}
+					break;
+				
+				case 's': case 'S':
+					if (posPlayer[a][b] == 1 && a < 2) {
+						posPlayer[a][b] = 0;
+						posPlayer[a + 1][b] = 1; 
+					}
+					break;
+						
+				default:
+					break;
+			}
+				
+//			if (posPlayer[a][b] == 1) {
+//				coordenadasPlayer[0] = a;
+//				coordenadasPlayer[1] = b;
+//				desenhaTela();
+//				return;
+//			}
+		}
+		
+	}
+
+	
+	for (int a = 0; a < 3; a++) {
+		for (int b = 0; b < 3; b++) {
+			printf("%d ", posPlayer[a][b]);
+		}
+		printf("\n");
+	}
+	printf("\n\n");
 }
 
 void conf_tela() {
