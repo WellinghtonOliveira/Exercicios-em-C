c8e995c7a162695b03b0067f0944c099c66a4a32
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2026-01-02 16:44:35 -0300

    Update desenhando quadrado.c

--- Exercicios/Testes/desenhando quadrado.c
+++ Exercicios/Testes/desenhando quadrado.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+
+int main() {
+	
+	int n;
+	
+	printf("Digite um numero correspondente ao tamanho do desenho ");
+	scanf("%d", &n);
+	
+	if (n < 0 || n == NULL) return 0;
+	
+	for (int a = 0; a < n; a++) {
+		for (int b = 0; b < n; b++) {
+			if (a == 0 || a == n - 1 ||
+				b == 0 || b == n - 1) {
+				printf(" #");
+			}else {
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
